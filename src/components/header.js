e945e11ce01bcf88f60b76654ed1aea595c883ba
Jimmy Zhen <hypeway@me.com>
2018-04-29 20:51:43 -0700

    Refactor <Header /> to use 'title' prop

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { PageHeader } from 'react-bootstrap';
 
-const Header = () => <PageHeader>Studio Ghibli Project</PageHeader>;
+const Header = props => <PageHeader>{props.title}</PageHeader>;
+
+Header.propTypes = {
+    title: PropTypes.string
+}
+
+Header.defaultProps = {
+    title: 'Code Project'
+}
 
 export default Header;
