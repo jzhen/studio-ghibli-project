60bacab33119d2d872ef6d3e556f6fe0ed5e655f
Jimmy Zhen <hypeway@me.com>
2018-04-30 23:30:23 -0700

    Add hanlding for a user to favorite items and render a list of user's favorites

--- src/components/list.js
+++ src/components/list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Grid, Row, Col, Button } from 'react-bootstrap';
+import Favorites from './favorites';
 
 class CategoryItems extends Component {
     constructor(props) {
@@ -8,7 +9,8 @@ class CategoryItems extends Component {
         this.state = {
             categoryItems: undefined,
             filteredList: undefined,
-            filterTerm: ''
+            filterTerm: '',
+            favorites: []
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -19,7 +21,6 @@ class CategoryItems extends Component {
             this.setState({
                 categoryItems: nextProps.selectedCategory,
                 filteredList: nextProps.selectedCategory
-                
             });
         }
     }
@@ -28,7 +29,7 @@ class CategoryItems extends Component {
      * Method to handle filter input change
      */
     handleChange(e) {
-        this.setState({filterTerm: e.target.value}, () => {
+        this.setState({ filterTerm: e.target.value }, () => {
             let items = this.state.categoryItems;
             let filterTerm = this.state.filterTerm;
             if (filterTerm && filterTerm.length) {
@@ -39,11 +40,21 @@ class CategoryItems extends Component {
                         return item.name.toLowerCase().indexOf(filterTerm.toLowerCase()) > -1;
                     }
                 });
-                this.setState({filteredList: filteredItems});
+                this.setState({ filteredList: filteredItems });
             } else {
-                this.setState({filteredList: this.state.categoryItems});
+                this.setState({ filteredList: this.state.categoryItems });
             }
         });
+    }
+
+    /**
+     * Method to handle click events of the add-to-favorite buttons
+     * @param {object} item - The category item
+     */
+    handleClick(item) {
+        let favorites  = this.state.favorites;
+        favorites.push(item);
+        this.setState({ favorites: favorites });
     }
 
     /**
@@ -57,7 +68,10 @@ class CategoryItems extends Component {
                     <ListGroupItem
                         header={item.title ? item.title : item.name}
                         key={item.id}>
-                        {item.description ? item.description : item.classification}
+                        <span className="description">{item.description ? item.description : item.classification}</span>
+                        <span className="favorite-action">
+                            <Button bsStyle="info" bsSize="xsmall" onClick={this.handleClick.bind(this, item)}>Add to favorites</Button>
+                        </span>
                     </ListGroupItem>
                 );
             });
@@ -71,18 +85,25 @@ class CategoryItems extends Component {
 
     render() {
         return (
-            <div>
-                <div className="filter-field">
-                    <input type="text" value={this.state.filterTerm} name="filterTerm" id="filterTerm"
-                        placeholder="Filter by title or name" onChange={this.handleChange}
-                        disabled={!this.state.categoryItems} className="form-control" />
-                </div>
-                <div className="category-item-list">
-                    <ListGroup>
-                        {this.renderCategoryList(this.state.filteredList)}
-                    </ListGroup>
-                </div>
-            </div>
+            <Grid>
+                <Row className="show-grid">
+                    <Col xs={12} md={8}>
+                        <div className="filter-field">
+                            <input type="text" value={this.state.filterTerm} name="filterTerm" id="filterTerm"
+                                placeholder="Filter by title or name" onChange={this.handleChange}
+                                disabled={!this.state.categoryItems} className="form-control" />
+                        </div>
+                        <div className="category-item-list">
+                            <ListGroup>
+                                {this.renderCategoryList(this.state.filteredList)}
+                            </ListGroup>
+                        </div>
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <Favorites favorites={this.state.favorites} />
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
